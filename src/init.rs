9f05d768aa3de4d7bec3745018c32d306042ad32
Josh Bechard <josh@oshbec.dev>
2019-09-08 17:27:25 -0500

    Init gets its own file #1

---
+++ src/init.rs
@@ -0,0 +1,40 @@
+
+use std::fs;
+use std::path::PathBuf;
+
+pub fn run<P: Into<PathBuf>>(directory: P) {
+    let mut directory = directory.into();
+    directory.push(".git");
+    fs::create_dir_all(&directory).unwrap();
+    directory.push("objects");
+    fs::create_dir_all(&directory).unwrap();
+    directory.pop();
+    directory.push("refs");
+    fs::create_dir_all(&directory).unwrap();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::{env, fs};
+
+    #[test]
+    fn initializes_repository_in_new_directory() {
+        // ARRANGE
+        let mut directory = env::temp_dir();
+        directory.push("repo");
+        let directory = directory.as_path();
+        // ACT
+        assert_eq!(directory.is_dir(), false);
+        run(&directory);
+        // ASSERT
+        for check_directory in &[".git", ".git/refs", ".git/objects"] {
+            let mut expected_directory = PathBuf::from(directory);
+            expected_directory.push(check_directory);
+            assert!(expected_directory.is_dir());
+        }
+        // CLEANUP
+        fs::remove_dir_all(directory).unwrap();
+        assert_eq!(directory.is_dir(), false);
+    }
+}
