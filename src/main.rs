0abb70e280d7de8e0e9a75f6f9c9c9ef066b20d0
Josh Bechard <josh@oshbec.dev>
2019-09-08 15:43:55 -0500

    A messy first try at  #1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
+use clap::{App, Arg, SubCommand};
+use std::fs;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("grit")
+        .version("1.0")
+        .about("A rust implementation of git")
+        .author("Josh Bechard")
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("Create an empty Git repository or reinitialize an existing one")
+                .arg(
+                    Arg::with_name("directory")
+                        .help("Where the repository lives")
+                        .default_value("."),
+                ),
+        )
+        .get_matches();
+
+    if let Some(init) = matches.subcommand_matches("init") {
+        if let Some(directory) = init.value_of("directory") {
+            let mut directory = PathBuf::from(directory);
+            directory.push(".git");
+            fs::create_dir_all(&directory).unwrap();
+            directory.push("objects");
+            fs::create_dir_all(&directory).unwrap();
+            directory.pop();
+            directory.push("refs");
+            fs::create_dir_all(&directory).unwrap();
+        }
+    }
 }
 
 #[cfg(test)]
