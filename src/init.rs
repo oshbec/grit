8b2eac5a1936c7087ef1840acb1946f3075d0d5f
Josh Bechard <josh@oshbec.dev>
2019-09-08 18:07:39 -0500

    Replaced all 'unwrap' with more helpful 'expect' #1

--- src/init.rs
+++ src/init.rs
@@ -6,7 +6,8 @@ pub fn run<P: AsRef<Path>>(directory: P) {
     let required_directories = &[".git", ".git/objects", ".git/refs"];
     for required_directory in required_directories {
         let required_directory = directory.join(required_directory);
-        fs::create_dir_all(required_directory).unwrap();
+        fs::create_dir_all(required_directory)
+            .expect("Could'nt create a required repository directory")
     }
 }
 
@@ -28,7 +29,7 @@ mod test {
             assert!(expected_directory.is_dir());
         }
         // CLEANUP
-        fs::remove_dir_all(&directory).unwrap();
+        fs::remove_dir_all(&directory).expect("Couldn't clean up the temp repository");
         assert_eq!(directory.is_dir(), false);
     }
 
@@ -36,7 +37,7 @@ mod test {
     fn initializes_repository_in_an_existing_directory() {
         // ARRANGE
         let directory = env::temp_dir().join("some_dir");
-        fs::create_dir(&directory).unwrap();
+        fs::create_dir(&directory).expect("Couldn't create the temp repository");
         // ACT
         assert_eq!(directory.is_dir(), true);
         run(&directory);
@@ -46,7 +47,7 @@ mod test {
             assert!(expected_directory.is_dir());
         }
         // CLEANUP
-        fs::remove_dir_all(&directory).unwrap();
+        fs::remove_dir_all(&directory).expect("Couldn't clean up the temp repository");
         assert_eq!(directory.is_dir(), false);
     }
 }
