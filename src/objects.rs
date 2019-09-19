6546016e294f5a20d27af29fb493c9ef9b8cc8ea
Josh Bechard <josh@oshbec.dev>
2019-09-19 12:57:00 -0500

    Add some automated tests to object / storage #3

--- src/objects.rs
+++ src/objects.rs
@@ -9,7 +9,7 @@ use libflate;
 
 pub struct Object {
     id: String,
-    contents: String,
+    _contents: String,
     entry: String,
 }
 
@@ -31,7 +31,7 @@ impl Object {
         let id = id_from_entry(&entry);
         Object {
             id,
-            contents,
+            _contents: contents,
             entry,
         }
     }
@@ -51,6 +51,15 @@ impl Object {
         path_to_file
     }
 
+    pub fn compress(&self) -> Vec<u8> {
+        let mut encoder = libflate::zlib::Encoder::new(Vec::new()).unwrap();
+        io::copy(&mut self.entry.to_owned().as_bytes(), &mut encoder).unwrap();
+        encoder
+            .finish()
+            .into_result()
+            .expect("Couldn't get encoding result")
+    }
+
     pub fn write(&self) -> Result<(), io::Error> {
         let path_to_entry = self.path();
         let directory = path_to_entry
@@ -58,15 +67,76 @@ impl Object {
             .expect("Couldn't get parent directory for db object");
         let tmp_object_path = directory.join(format!("tmp_object_{}", Uuid::new_v4()));
         fs::create_dir_all(directory)?;
-        let mut tmp_object_file = fs::File::create(&tmp_object_path)?;
-        let mut encoder = libflate::zlib::Encoder::new(Vec::new()).unwrap();
-        io::copy(&mut self.entry.to_owned().as_bytes(), &mut encoder).unwrap();
-        let encoded_data = encoder
-            .finish()
-            .into_result()
-            .expect("Couldn't get encoding result");
-        tmp_object_file.write_all(&encoded_data)?;
+        let encoded_data = self.compress();
+        fs::File::create(&tmp_object_path)?.write_all(&encoded_data)?;
         fs::rename(tmp_object_path, path_to_entry)?;
         Ok(())
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn builds_entry_from_contents() {
+        let contents = "This is some great content".to_string();
+        let object = Object::from_contents(contents);
+        assert_eq!(
+            object.entry,
+            "blob 26\0This is some great content".to_string()
+        );
+    }
+
+    #[test]
+    fn calculates_sha1_id_from_entry() {
+        let contents = "This is some great content".to_string();
+        let object = Object::from_contents(contents);
+        assert_eq!(
+            object.id,
+            "a8be488abce200ee4f988c2a63ed5a61f8362521".to_string()
+        );
+    }
+
+    #[test]
+    fn compresses_the_entry() {
+        // This is sort of a snapshot-style test
+        let contents = "This is some great content".to_string();
+        let object = Object::from_contents(contents);
+        assert_eq!(
+            object.compress(),
+            vec![
+                120, 156, 13, 194, 81, 9, 0, 32, 12, 4, 80, 163, 92, 6, 63, 76, 98, 1, 39, 67, 5,
+                221, 192, 93, 127, 20, 158, 108, 23, 228, 146, 234, 92, 129, 47, 252, 40, 198, 213,
+                70, 116, 55, 170, 241, 1, 195, 52, 11, 222,
+            ]
+        );
+    }
+
+    #[test]
+    fn knows_where_to_save_from_id() {
+        let contents = "This is some great content".to_string();
+        let object = Object::from_contents(contents);
+
+        // Just showing that we know the full SHA1 hash to base expected path from
+        assert_eq!(
+            object.id,
+            "a8be488abce200ee4f988c2a63ed5a61f8362521".to_string()
+        );
+
+        // first two characters of sha1
+        let directory = "a8".to_string();
+        // remaining characters of sha1
+        let file = "be488abce200ee4f988c2a63ed5a61f8362521".to_string();
+
+        let expected_path = env::current_dir()
+            .unwrap()
+            .join(".git")
+            .join("objects")
+            .join(directory)
+            .join(file);
+
+        assert_eq!(object.path(), expected_path);
+    }
+
 }
