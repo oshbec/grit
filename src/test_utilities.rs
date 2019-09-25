0bea0621fbc8fdd39b6c0af7d81be6b0b824580c
Josh Bechard <josh@oshbec.dev>
2019-09-24 21:15:56 -0500

    Change original_working_directory to original_current_dir

--- src/test_utilities.rs
+++ src/test_utilities.rs
@@ -5,23 +5,23 @@ use uuid::Uuid;
 
 pub struct TestWorkspace {
     pub directory: PathBuf,
-    original_working_directory: PathBuf,
+    original_current_dir: PathBuf,
 }
 
 impl TestWorkspace {
     pub fn setup() -> TestWorkspace {
         let workspace_path = path_to_temporary_workspace();
         fs::create_dir_all(&workspace_path).expect("Couldn't create temporary workspace directory");
-        let original_working_directory = env::current_dir().unwrap();
+        let original_current_dir = env::current_dir().unwrap();
         env::set_current_dir(&workspace_path).expect("Couldn't set CWD to temp workspace");
         TestWorkspace {
             directory: workspace_path,
-            original_working_directory,
+            original_current_dir,
         }
     }
 
     pub fn teardown(&self) {
-        env::set_current_dir(&self.original_working_directory)
+        env::set_current_dir(&self.original_current_dir)
             .expect("Couldn't reset CWD in workspace teardown");
         fs::remove_dir_all(&self.directory).expect("Couldn't delete the repository directory");
     }
