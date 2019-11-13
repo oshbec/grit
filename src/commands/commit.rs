53e9bc35a3b6d9db2160294887ce63636e9a6528
Josh Bechard <josh@oshbec.dev>
2019-11-13 11:44:05 -0600

    Slightly better organization

--- src/commands/commit.rs
+++ src/commands/commit.rs
@@ -10,6 +10,7 @@ pub fn run(message: &str) -> Result<String, String> {
     let current_dir = env::current_dir().expect("Couldn't determine current directory");
     let ignore: Ignore = Default::default();
     let files_to_commit = list_files(&current_dir, &ignore);
+
     let blobs: Vec<Blob> = files_to_commit
         .iter()
         .map(|file| {
@@ -18,11 +19,15 @@ pub fn run(message: &str) -> Result<String, String> {
             blob
         })
         .collect();
+
     let tree = Tree::from_blobs(blobs);
     objects::write(&tree).expect("Couldn't write tree to git database");
+
     let commit = Commit::new(tree.id(), message);
     objects::write(&commit).expect("Couldn't write the commit to git database");
+
     refs::update_head(&commit.id());
+
     Ok(commit.id())
 }
 
--- src/commands/init.rs
+++ src/commands/init.rs
@@ -4,10 +4,11 @@ use std::{env, fs, path::PathBuf};
 pub fn run(directory: Option<&PathBuf>) {
     let current_dir = env::current_dir().expect("Couldn't identify current working directory");
     let directory = directory.unwrap_or(&current_dir);
-    let required_directories = vec![".git", ".git/objects", ".git/refs"];
-    for required_directory in required_directories {
-        let required_directory = directory.join(required_directory);
-        fs::create_dir_all(required_directory).expect("Could not create required directory");
+
+    let required_git_directories = vec![".git", ".git/objects", ".git/refs"];
+    for required_git_directory in required_git_directories {
+        let required_git_directory = directory.join(required_git_directory);
+        fs::create_dir_all(required_git_directory).expect("Could not create required directory");
     }
     println!("Initialized the git repo at {:?}", &directory);
 }
